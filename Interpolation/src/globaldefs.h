49e7955574c01b3bef28d6cd3b23d112b6566219
rodriguezmeza <marioalberto.rodriguez@inin.gob.mx>
2022-01-31 13:09:03 -0600

    Update files

---
+++ Interpolation/src/globaldefs.h
@@ -0,0 +1,132 @@
+/*==============================================================================
+ HEADER: globaldefs.h		[interpolation]
+ ==============================================================================*/
+
+#ifndef _globaldefs_h
+#define _globaldefs_h
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+//
+
+#include "general_libs/stdinc.h"
+#include "general_libs/numrec.h"
+#include "general_libs/interpolation.h"
+#include "general_libs/mathfns.h"
+#include "general_libs/mathutil.h"
+#include "general_libs/inout.h"
+#include "general_libs/getparam.h"
+
+#if !defined(global)
+#  define global extern
+#endif
+
+#define IPName(param,paramtext)    \
+{strcpy(tag[nt],paramtext);    \
+addr[nt]=&(param);    \
+id[nt++]=INT;}
+
+#define RPName(param,paramtext)    \
+{strcpy(tag[nt],paramtext);    \
+addr[nt]=&param;    \
+id[nt++]=DOUBLE;}
+
+#define BPName(param,paramtext)    \
+{strcpy(tag[nt],paramtext);    \
+addr[nt]=&param;    \
+id[nt++]=BOOLEAN;}
+
+#define SPName(param,paramtext,n)    \
+{strcpy(tag[nt],paramtext);    \
+param=(string) malloc(n);    \
+addr[nt]=param;    \
+id[nt++]=STRING;}
+//
+
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FOURPI2   39.4784176043574
+#define PI2     9.8696044010893586188
+#define TWOPI2     19.739208802178716
+#define SIXPI2  59.21762640653615
+#define INVSQRTDTWOPI 0.39894228040143267794
+
+typedef struct {
+
+// Power spectrum table
+    string fnamePS;
+    real kmin;
+    real kmax;
+    int Nk;
+//
+    string options;
+//
+    string paramfile;
+//
+} cmdline_data, *cmdline_data_ptr;
+
+typedef struct {
+	real cpuinit;
+
+	string headline0;
+	string headline1;
+	string headline2;
+	string headline3;
+
+	FILE *outlog;
+
+	char mode[2];
+
+// 
+    char fnamePSPath[100];
+    char logfilePath[100];
+    char clptDir[100];
+    char tmpDir[100];
+    char fpfnamekfun[100];
+
+} global_data, *global_data_ptr;
+
+
+global global_data gd;
+global cmdline_data cmd;
+
+typedef struct _pointPSTable {
+    real k;
+    real ps;
+} pointPSTable, *pointPSTableptr;
+
+global int nPSTable;
+global pointPSTableptr PSLCDMtab;
+global int nPSLogT;
+global pointPSTableptr PSLCDMLogtab;
+
+global int nPSLT;
+global pointPSTableptr PSLT;
+global int nPSLTLog;
+global pointPSTableptr PSLTLog;
+
+global real *kPS;
+global real *pPS;
+global real *pPS2;
+
+
+#define kPos(x)    (((pointPSTableptr) (x))->k)
+#define PS(x)    (((pointPSTableptr) (x))->ps)
+
+// STATIC problem: gcc version 11
+// From inout.h
+global real *inout_xval;
+global real *inout_yval;
+global real *inout_zval;
+global real *inout_wval;
+
+// STATIC problem: gcc version 11
+// From stdinc.h
+global long idum;                // seed for random generators
+
+#endif // ! _globaldefs_h
+
---
+++ Interpolation/src/interpol.c
@@ -0,0 +1,278 @@
+/*==============================================================================
+ MODULE: interpol.c				[interpolation]
+==============================================================================*/
+
+#include "globaldefs.h"
+#include "protodefs.h"
+
+local void InputPSTable(void);
+local void PSLTable(void);
+
+void MainLoop(void)
+{
+    if (!strnull(cmd.fnamePS)) {
+        sprintf(gd.fnamePSPath,"Input/%s",cmd.fnamePS);
+        fprintf(gd.outlog,"PS file Path and file name: %s\n",gd.fnamePSPath);
+        fflush(gd.outlog);
+        InputPSTable();
+        PSLTable();
+    }
+}
+
+
+#define NPT 10
+#define SPREAD 1.0
+local void InputPSTable(void)
+{
+    stream outstr;
+    pointPSTableptr p, plog, pn;
+    pointPSTableptr PSLCDMtabtmp;
+    int nPSTabletmp;
+    int i;
+    real dk, kval, PSval, kmin, kmax;
+    int mwt;
+    double al,bl,chi2,q,siga,sigb,*x,*y,*sig;
+    double au, bu;
+    real *kPStmp;
+    real *pPStmp;
+    real *pPS2tmp;
+    char namebuf[256];
+    real kminext, kmaxext, dktmp, kmn, kmx;
+    int Nkext=800, NkL=50, NkU=50;
+    real kminT=1.0e-5, kmaxT=400.0;
+
+    fprintf(gd.outlog,"\n\nReading power spectrum from file %s...\n",gd.fnamePSPath);
+    inout_InputData(gd.fnamePSPath, 1, 2, &nPSTabletmp);
+
+    if (nPSTabletmp < 1)
+        error("\n\nInputPSTable: nPSTable = %d is absurd\n\n", nPSTabletmp);
+
+    PSLCDMtabtmp = (pointPSTableptr) allocate(nPSTabletmp * sizeof(pointPSTable));
+    
+    fprintf(gd.outlog,"nPSTable : %d\n", nPSTabletmp);
+
+    i = 0;
+    for (p=PSLCDMtabtmp; p<PSLCDMtabtmp+nPSTabletmp; p++) {
+        kPos(p) = inout_xval[i];
+        PS(p) = inout_yval[i];
+        ++i;
+    }
+
+    fprintf(gd.outlog,"\n\nCreating log power spectrum...\n");
+
+    PSLCDMLogtab = (pointPSTableptr) allocate(nPSTabletmp * sizeof(pointPSTable));
+    plog = PSLCDMLogtab;
+    nPSLogT=0;
+    for (p=PSLCDMtabtmp; p<PSLCDMtabtmp+nPSTabletmp; p++) {
+        kPos(plog) = rlog10(kPos(p));
+        PS(plog) = rlog10(PS(p));
+        plog++;
+        nPSLogT++;
+    }
+
+    fprintf(gd.outlog,"\nTotal numbers in Log PS: %d %d\n",nPSLogT,plog-PSLCDMLogtab);
+    fprintf(gd.outlog,"Total numbers in Normal PS: %d %d\n\n",nPSTabletmp,p-PSLCDMtabtmp);
+
+    fprintf(gd.outlog,"\n\nLinear fit (a + b x) to log-log power spectrum at minset and maxset...\n");
+    fprintf(gd.outlog,"\n\nLinear fit to log-log power spectrum at minset and maxset...\n");
+
+    x=dvector(1,NPT);
+    y=dvector(1,NPT);
+    sig=dvector(1,NPT);
+    
+// Lower part of the PS
+    fprintf(gd.outlog,"\nAt lower part of the spectrum...\n");
+
+    plog = PSLCDMLogtab;
+    for (i=1;i<=NPT;i++) {
+        x[i]=kPos(plog);
+        y[i]=PS(plog);
+        sig[i]=SPREAD;
+        plog++;
+    }
+    for (mwt=0;mwt<=1;mwt++) {
+        fit(x,y,NPT,sig,mwt,&al,&bl,&siga,&sigb,&chi2,&q);
+        if (mwt == 0)
+            fprintf(gd.outlog,"\nIgnoring standard deviations\n");
+        else
+            fprintf(gd.outlog,"\nIncluding standard deviations\n");
+        fprintf(gd.outlog,"%12s %9.6f %18s %9.6f \n",
+               "a  =  ",al,"uncertainty:",siga);
+        fprintf(gd.outlog,"%12s %9.6f %18s %9.6f \n",
+               "b  =  ",bl,"uncertainty:",sigb);
+        fprintf(gd.outlog,"%19s %14.6f \n","chi-squared: ",chi2);
+        fprintf(gd.outlog,"%23s %10.6f \n","goodness-of-fit: ",q);
+    }
+
+// Upper part of the PS
+    fprintf(gd.outlog,"\nAt upper part of the spectrum...\n");
+
+    plog = PSLCDMLogtab+nPSLogT-1;
+    for (i=1;i<=NPT;i++) {
+        x[i]=kPos(plog);
+        y[i]=PS(plog);
+        sig[i]=SPREAD;
+        plog--;
+    }
+    for (mwt=0;mwt<=1;mwt++) {
+        fit(x,y,NPT,sig,mwt,&au,&bu,&siga,&sigb,&chi2,&q);
+        if (mwt == 0)
+            fprintf(gd.outlog,"\nIgnoring standard deviations\n");
+        else
+            fprintf(gd.outlog,"\nIncluding standard deviations\n");
+        fprintf(gd.outlog,"%12s %9.6f %18s %9.6f \n",
+               "a  =  ",au,"uncertainty:",siga);
+        fprintf(gd.outlog,"%12s %9.6f %18s %9.6f \n",
+               "b  =  ",bu,"uncertainty:",sigb);
+        fprintf(gd.outlog,"%19s %14.6f \n","chi-squared: ",chi2);
+        fprintf(gd.outlog,"%23s %10.6f \n","goodness-of-fit: ",q);
+    }
+
+// Extending power spectrum
+    kPStmp = dvector(1,nPSLogT);
+    pPStmp = dvector(1,nPSLogT);
+    pPS2 = dvector(1,nPSLogT);
+    pn = PSLCDMLogtab;
+    i=1;
+    for (pn = PSLCDMLogtab; pn<PSLCDMLogtab+nPSLogT; pn++) {
+        kPStmp[i] = kPos(pn);
+        pPStmp[i] = PS(pn);
+        i++;
+    }
+//
+    spline(kPStmp,pPStmp,nPSLogT,1.0e30,1.0e30,pPS2);
+
+    kmin = kPos(PSLCDMtabtmp);
+    kmax = kPos(PSLCDMtabtmp+nPSTabletmp-1);
+    fprintf(gd.outlog,"\nkmin, kmax of the given power spectrum (with %d values): %g %g",kmin, kmax, nPSTabletmp);
+    dktmp = (rlog10(kmax) - rlog10(kmin))/((real)(nPSTabletmp - 1));
+    kminext = rpow(10.0, rlog10(kmin)-((real)NkL)*dktmp);
+    kmaxext = rpow(10.0, rlog10(kmax)+((real)NkU)*dktmp);
+//
+    fprintf(gd.outlog,"\n\nNkL, NkU: %d %d\n",NkL, NkU);
+    NkL = rlog10(kmin/kminT)/dktmp;
+    NkU = rlog10(kmaxT/kmax)/dktmp;
+    fprintf(gd.outlog,"\n\nNkL, NkU targets: %d %d\n",NkL,NkU);
+    kminext = rpow(10.0, rlog10(kmin)-((real)NkL)*dktmp);
+    kmaxext = rpow(10.0, rlog10(kmax)+((real)NkU)*dktmp);
+//
+
+    fprintf(gd.outlog,"\nkmin, kmax of the extended power spectrum (first try): %g %g",
+            kminext, kmaxext);
+
+    kmn = MIN(kminext,cmd.kmin);
+    kmx = MAX(kmaxext,cmd.kmax);
+    fprintf(gd.outlog,"\nkmin, kmax of the extended power spectrum (second try): %g %g\n",
+            kmn, kmx);
+
+    nPSTable = Nkext;
+    fprintf(gd.outlog,"\n\nCreating new PSTable with %d values\n",nPSTable);
+    PSLCDMtab = (pointPSTableptr) allocate(nPSTable * sizeof(pointPSTable));
+    dk = (rlog10(kmx) - rlog10(kmn))/((real)(nPSTable - 1));
+    p = PSLCDMtab;
+    
+    for (i=1; i<=nPSTable; i++) {
+        kval = rlog10(kmn) + dk*((real)(i - 1));
+        if (rpow(10.0,kval) >= kmin && rpow(10.0,kval) <= kmax)
+            PSval = psInterpolation_nr(kval, kPStmp, pPStmp, nPSLogT);
+        else
+            if (rpow(10.0,kval) < kmin)
+                PSval = al + bl*kval;
+            else
+                if (rpow(10.0,kval) > kmax)
+                    PSval = au + bu*kval;
+                else
+                    error("\n\nError: InputPSTable :: kmin, kmax, kval: %g %g %g", kmin, kmax, kval);
+//
+        kPos(p) = rpow(10.0,kval);
+        PS(p) = rpow(10.0,PSval);
+        p++;
+    }
+
+//    sprintf(namebuf,"%s/%s%s_%s",gd.tmpDir,cmd.fnamePS,cmd.suffixModel,"ext.dat");
+    sprintf(namebuf,"%s/%s_%s",gd.tmpDir,cmd.fnamePS,"ext.dat");
+    outstr = stropen(namebuf,"w!");
+    for (p=PSLCDMtab; p<PSLCDMtab+nPSTable; p++) {
+        fprintf(outstr,"%g %g\n",
+                kPos(p),PS(p));
+    }
+    fclose(outstr);
+//
+
+    free_dvector(pPS2,1,nPSLogT);
+    free_dvector(pPStmp,1,nPSLogT);
+    free_dvector(kPStmp,1,nPSLogT);
+
+    free_dvector(sig,1,NPT);
+    free_dvector(y,1,NPT);
+    free_dvector(x,1,NPT);
+}
+#undef NPT
+#undef SPREAD
+
+
+local void PSLTable(void)
+{
+    char namebuf[256];
+    stream outstr;
+    real kmin, Dpkmin, Dpk;
+    pointPSTableptr p, pn;
+    int i;
+//
+    real xstoptmp, Dp0, Dpzout, fac;
+
+    sprintf(namebuf,"%s/%s_%s",gd.tmpDir,cmd.fnamePS,"ext2.dat");
+    outstr = stropen(namebuf,"w!");
+    for (p=PSLCDMtab; p<PSLCDMtab+nPSTable; p++) {
+        fprintf(outstr,"%g %g\n",
+                kPos(p),PS(p));
+    }
+    fclose(outstr);
+//
+    PSLT = (pointPSTableptr) allocate(nPSTable * sizeof(pointPSTable));
+    nPSLT = 0;
+    pn = PSLT;
+    for (p = PSLCDMtab; p<PSLCDMtab+nPSTable; p++) {
+        kPos(pn) = kPos(p);
+        PS(pn) = PS(p);             // Dummy line
+        pn++;
+        nPSLT++;
+    }
+
+
+    kPS = dvector(1,nPSLT);
+    pPS = dvector(1,nPSLT);
+    pPS2 = dvector(1,nPSLT);
+    pn = PSLT;
+    i=1;
+    for (pn = PSLT; pn<PSLT+nPSLT; pn++) {
+        kPS[i] = kPos(pn);
+        pPS[i] = PS(pn);
+        i++;
+    }
+//
+    spline(kPS,pPS,nPSLT,1.0e30,1.0e30,pPS2);
+
+    sprintf(namebuf,"%s/%s%s",gd.clptDir,"PSL",".dat");
+    outstr = stropen(namebuf,"w!");
+    for (i=1; i<=nPSLT; i++) {
+        fprintf(outstr,"%e %e\n",
+                kPS[i],pPS[i]);
+    }
+    fclose(outstr);
+}
+
+
+global  real psInterpolation_nr(real k, double kPS[], double pPS[], int nPS)
+{
+//    pointPSTableptr pf, pi;
+    real psftmp;
+//    real dps;
+    
+//    pi = PSLCDMtab;
+//    pf = PSLCDMtab+nPSTable-1;
+    
+    splint(kPS,pPS,pPS2,nPS,k,&psftmp);
+    
+    return (psftmp);
+}
